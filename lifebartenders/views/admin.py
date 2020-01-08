6e39ccb3a1bd11dac0950feabf8b45a6d5b3e654
Leandro Santos <leandro.admo@gmail.com>
2020-01-08 17:25:37 -0300

    navigation panel added

--- lifebartenders/views/admin.py
+++ lifebartenders/views/admin.py
@@ -7,3 +7,11 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 @admin.route('/')
 def dashboard():
     return render_template('admin/dashboard.html')
+
+@admin.route('/agenda')
+def agenda():
+    return render_template('admin/agenda.html')
+
+@admin.route('/eventos')
+def eventos():
+    return render_template('admin/eventos.html')
