6e16d4e7a1bc0e2114f57f6aefa33ab3acb575b3
leandro <leandro.admo@gmail.com>
2020-03-13 09:40:45 -0300

    sender email configured to application

--- lifebartenders/__init__.py
+++ lifebartenders/__init__.py
@@ -14,13 +14,6 @@ if os.path.isfile(dotenv_path):
     from dotenv import load_dotenv
     load_dotenv(dotenv_path)
 
-# app = Flask(__name__)
-# app.config.from_object('lifebartenders.config')
-
-# db = SQLAlchemy(app)
-# mail = Mail(app)
-# images = Images(app)
-# user_manager = UserManager(app, db, 'User')
 db = SQLAlchemy()
 mail = Mail()
 images = Images()
@@ -37,8 +30,6 @@ def create_app():
     return app
 
 app = create_app()
-
-# print(app.__dict__)
 
 handler = logging.StreamHandler(sys.stdout)
 if not app.config.get('DEBUG'):
