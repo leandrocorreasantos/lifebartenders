8c9eba1c636d0ef3e83399bed2e333d9230ba1f4
leandro <leandro.admo@gmail.com>
2020-03-13 08:16:20 -0300

    update name from app

--- lifebartenders/__init__.py
+++ lifebartenders/__init__.py
@@ -14,7 +14,7 @@ if os.path.isfile(dotenv_path):
     from dotenv import load_dotenv
     load_dotenv(dotenv_path)
 
-app = Flask('lifebartenders')
+app = Flask(__name__)
 app.config.from_object('lifebartenders.config')
 
 db = SQLAlchemy(app)
